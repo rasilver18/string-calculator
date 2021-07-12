1ea6decef61576b96578b6481aed9c8200becbdf
Aaron Burne <rasilver18@gmail.com>
2021-07-12 12:29:16 -0400

    initial tests written

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,2 +1,10 @@
 public class StringCalculator {
+    public static int sum(String calculation){
+        if(calculation.isEmpty()){
+            return 0;
+        }
+        else{
+        return Integer.parseInt(calculation);
+        }
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+    StringCalculator calculator = new StringCalculator();
+    @Test
+    public void stringShouldReturnZeroWhenEmpty(){
+        assertEquals(calculator.sum(""), 0);
+
+
+    }
+    @Test
+    public void stringShouldReturnSingleNumber(){
+        assertEquals(calculator.sum("7"), 7);
+    }
 }
