dd9b525c66cd4cf36997fc69616984521c8b363d
Aaron Burne <rasilver18@gmail.com>
2021-07-12 14:53:26 -0400

    realized cleaner way to write. Completed tasks 3 and 4

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,14 +1,27 @@
 public class StringCalculator {
-    public static int sum(String calculation){
+    public int add(String calculation){
+        String [] numbers = calculation.split(",|\n");
         if(calculation.isEmpty()){
             return 0;
         }
-        else if(calculation.contains(",")){
-            String [] numbers = calculation.split(",");
-            return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+        else if(numbers.length >1){
+            return calculatedSum(numbers);
+
+
         }
         else{
         return Integer.parseInt(calculation);
         }
+
     }
+    private int calculatedSum(String[] numbers){
+        int sum =0;
+        for(String number:numbers){
+
+            sum += Integer.parseInt(number);
+        }
+        return sum;
+    }
+
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,21 +1,44 @@
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
-    StringCalculator calculator = new StringCalculator();
+    private StringCalculator calculator;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void initialize() {
+        calculator = new StringCalculator();
+    }
     @Test
     public void stringShouldReturnZeroWhenEmpty(){
-        assertEquals(calculator.sum(""), 0);
+        assertEquals(calculator.add(""), 0);
 
 
     }
     @Test
     public void stringShouldReturnSingleNumber(){
-        assertEquals(calculator.sum("7"), 7);
+        assertEquals(calculator.add("7"), 7);
     }
     @Test
     public void stringShouldSumTwoNumbersSeparatedByComma(){
-        assertEquals(calculator.sum("2,3"), 5);
+        assertEquals(calculator.add("2,3"), 5);
+    }
+
+    @Test
+    public void stringShouldReturnSumOfMultipleNumbers(){
+
+        assertEquals(calculator.add("3,3,3"), 9);
+    }
+
+    @Test
+    public void stringShouldReturnSumOfNumbersOnNewLine(){
+        assertEquals(calculator.add("5\n5\n5"), 15);
     }
 }
+
